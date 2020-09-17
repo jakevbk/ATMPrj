4600fcb16c16973d0d26548a738b409d621d9266
jakevbk <70787453+jakevbk@users.noreply.github.com>
2020-09-17 15:58:12 -0400

    I added GUI files. I did not work on them yet. We still need to figure out super method but I transferred his extra test cases over

--- src/atmPack/ATMTest.java
+++ src/atmPack/ATMTest.java
@@ -180,6 +180,7 @@ public class ATMTest {
 		assertFalse(ATM.equals(s1,s2));
 	}
 
+
 	@Test
 	public void compareToGreater(){
 		ATM s1 = new ATM(12, 0, 0);
@@ -234,7 +235,6 @@ public class ATMTest {
 		assertTrue(s1.equals(s2));
 
 	}
-
 
 	@Test
 	public void testMutate () {
@@ -311,5 +311,184 @@ public class ATMTest {
 	public void setNegativeHundreds(){
 		ATM s1 = new ATM(1,0,0);
 		s1.setHundreds(-1);
+	}
+
+	// testing not able to make change
+	@Test(expected = IllegalArgumentException.class)
+	public void testTakeOutNull1() {
+		ATM s1 = new ATM(3, 1, 2);
+		ATM s2 = s1.takeOut(700);
+		assertEquals(s2, null);
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void testSaveLoadError () {
+		ATM d = new ATM(1, 11, 2018);
+		d.save("-!@#$%^&*()testit-.txt");
+	}
+
+	public ATM myTakeOut(int totalAmount, int hundreds, int fifties, int twenties) {
+		if (totalAmount <= (hundreds * 100 + fifties * 50 + twenties * 20)) {
+			for (int a = hundreds; a >= 0; a--) {
+				for (int b = fifties; b >= 0; b--) {
+					for (int c = twenties; c >= 0; c--) {
+						if ((a * 100 + b * 50 + c * 20) == totalAmount) {
+							hundreds -= a;
+							fifties -= b;
+							twenties -= c;
+							return new ATM(a, b, c);
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void testequals2() {
+		ATM d1 = new ATM(2,21,2021);
+		d1.equals("pizza");
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void testequals3() {
+		ATM d1 = new ATM(2,29,2021);
+		d1.equals(null);
+	}
+
+	@Test
+	public void testTakeSuperTest() {
+		int amount = 20;
+		int numOfIll = 0;
+
+		for (int h = 0; h < amount; h++) {
+			System.out.println(h);
+			for (int f = 0; f < amount; f++)
+				for (int t = 0; t < amount; t++)
+					for (int m = 10; m < ((amount * 100) + (amount * 50) + (amount * 20) + 10); m += 10) {
+						try {
+							ATM a1 = new ATM(h, f, t);
+							ATM b1 = a1.takeOut(m);
+
+							ATM a2 = new ATM(h, f, t);
+							ATM b2 = myTakeOut(m, h, f, t);
+
+							//assertEquals (a1,a2);
+							assertEquals(b1, b2);
+						}
+						catch (IllegalArgumentException e) {
+							numOfIll++;
+						}
+					}
+		}
+		System.out.println(numOfIll);
+		assertEquals(1589500, numOfIll);
+	}
+
+	@Test
+	public void testTakeOut3() {
+		ATM s1 = new ATM(0, 50, 3);
+		ATM s2 = s1.takeOut(160);
+		assertEquals(s1.getHundreds(), 0);
+		assertEquals(s1.getFifties(), 48);
+		assertEquals(s1.getTwenties(), 0);
+
+		assertEquals(s2.getHundreds(), 0);
+		assertEquals(s2.getFifties(), 2);
+		assertEquals(s2.getTwenties(), 3);
+	}
+
+	@Test
+	public void testTakeOut4() {
+		ATM s1 = new ATM(5, 1, 4);
+		ATM s2 = s1.takeOut(180);
+		assertEquals(s1.getHundreds(), 4);
+		assertEquals(s1.getFifties(), 1);
+		assertEquals(s1.getTwenties(), 0);
+
+		assertEquals(s2.getHundreds(), 1);
+		assertEquals(s2.getFifties(), 0);
+		assertEquals(s2.getTwenties(), 4);
+	}
+
+	@Test
+	public void testTakeOut4a() {
+		ATM s1 = new ATM(1, 1, 4);
+		ATM s2 = s1.takeOut(130);
+		assertEquals(s1.getHundreds(), 1);
+		assertEquals(s1.getFifties(), 0);
+		assertEquals(s1.getTwenties(), 0);
+
+		assertEquals(s2.getHundreds(), 0);
+		assertEquals(s2.getFifties(), 1);
+		assertEquals(s2.getTwenties(), 4);
+	}
+
+	@Test
+	public void testTakeOut4b() {
+		ATM s1 = new ATM(5, 1, 4);
+		ATM s2 = s1.takeOut(180);
+		assertEquals(s1.getHundreds(), 4);
+		assertEquals(s1.getFifties(), 1);
+		assertEquals(s1.getTwenties(), 0);
+
+		assertEquals(s2.getHundreds(), 1);
+		assertEquals(s2.getFifties(), 0);
+		assertEquals(s2.getTwenties(), 4);
+	}
+
+	@Test
+	public void testTakeOut5() {
+		ATM s1 = new ATM(0, 1, 4);
+		ATM s2 = s1.takeOut(80);
+		assertEquals(s1.getHundreds(), 0);
+		assertEquals(s1.getFifties(), 1);
+		assertEquals(s1.getTwenties(), 0);
+
+		assertEquals(s2.getHundreds(), 0);
+		assertEquals(s2.getFifties(), 0);
+		assertEquals(s2.getTwenties(), 4);
+	}
+
+	@Test
+	public void testTakeOut6() {
+		ATM s1 = new ATM(0, 3, 3);
+		ATM s2 = s1.takeOut(160);
+		assertEquals(s1.getHundreds(), 0);
+		assertEquals(s1.getFifties(), 1);
+		assertEquals(s1.getTwenties(), 0);
+
+		assertEquals(s2.getHundreds(), 0);
+		assertEquals(s2.getFifties(), 2);
+		assertEquals(s2.getTwenties(), 3);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorNegHundereds() {
+		new ATM(-300, 0, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPutInNeghundereds() {
+		ATM s = new ATM(2, 3, 4);
+		s.putIn(-30, 2, 30);
+	}
+
+	public void TestItALL() {
+		for (int hundreds = 0; hundreds < 10; hundreds++)
+			for (int fifties = 0; fifties < 10; fifties++)
+				for (int twenties = 0; twenties < 10; twenties++)
+					for (int p = 0; p < 10; p++)
+						for (int amt = 20; amt < 500; amt += 10) {
+							ATM j1 = new ATM(hundreds, fifties, twenties);
+							ATM j2 = new ATM(hundreds, fifties, twenties);
+							ATM j3 = new ATM(hundreds, fifties, twenties);
+							ATM j4 = new ATM(hundreds, fifties, twenties);
+
+							try {
+								ATM j11 = j1.takeOut(amt);
+							} catch (Exception e) {
+							}
+						}
 	}
 }
